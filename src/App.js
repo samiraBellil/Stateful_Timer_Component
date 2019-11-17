55b7d2573674930ff5b05b5fc7374a667f6de485
samirabellil <samirabellil2014@gmail.com>
2019-11-17 15:26:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 0,
+      start: false
+    };
+    setInterval(
+      () => this.state.start ? this.setState({ timer: this.state.timer + 1000 })
+        : clearInterval(this.state.timer), 1000);
+  }
+  startCounting = () => {
+    this.setState({
+       start: !this.state.start });
+  };
+  render() {
+    var hours = Math.floor(this.state.timer / 3600000);
+    var minutes = Math.floor((this.state.timer - (hours * 3600000)) / 60000);
+    var seconds = parseInt((this.state.timer - (hours * 3600000) - (minutes * 60000)) / 1000);
+    return (
+      <div className="App">
+        <h1>
+          {String(hours).padStart(2, "0")} : {String(minutes).padStart(2, "0")}{" "}
+          : {String(seconds).padStart(2, "0")}
+        </h1>
+        <button onClick={this.startCounting}>{this.state.start ? "Pause" : "Start"}</button>
+        <button onClick={() => this.setState({ timer: 0, start: false })}>Reset</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+export default App;
